6463e6830b58e6779bccdd63097add495a66f609
martin <martin.rubiano1234@gmail.com>
2024-06-27 13:54:38 -0500

    Finishing project

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
+import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
@@ -9,7 +10,7 @@ import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} /> 
         <Hero />
@@ -18,6 +19,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach /> 
+        <Contact /> 
       </div>
     </main>
   );
